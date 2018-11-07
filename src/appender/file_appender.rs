8ce92ff82e9e82cc20e9e1fac4c42f8fe9223c2f
wtu-sos <595388976@qq.com>
2018-11-07 22:13:09 +0800

    new log file rule

--- src/appender/file_appender.rs
+++ src/appender/file_appender.rs
@@ -1,6 +1,7 @@
 use std::fs::{self, File};
-use std::fs::OpenOptions;
+use std::fs::{OpenOptions, DirBuilder};
 use std::io::Write;
+use std::path::{PathBuf};
 use crate::appender::Appender;
 
 pub struct FileAppender {
@@ -9,15 +10,16 @@ pub struct FileAppender {
     buf: String,
     log_max_size: u64,
     roll_index: u32,
-    base_name: String,
+    base_dir: String,
     file_name: String,
     file: Option<File>,
 }
 
 impl FileAppender {
-    pub fn new(buf_size: usize, base_name: String) -> Self {
+    pub fn new(buf_size: usize, base_dir: String) -> Self {
         // todo : check current index
-        let log_name = FileAppender::get_file_name(&base_name, 0);
+        let _ = DirBuilder::new().recursive(true).create(base_dir.clone());
+        let log_name = FileAppender::get_file_name(&base_dir, 0);
         let file = OpenOptions::new().create(true)
                                     .write(true)
                                     .append(true)
@@ -29,16 +31,18 @@ impl FileAppender {
             buf: String::with_capacity(buf_size),
             log_max_size: 1024u64*1024u64*512,
             roll_index: 0,
-            base_name,
+            base_dir,
             file_name: log_name,
             file: Some(file),
         }
     }
 
-    fn get_file_name(file_name: &String, idx: u32) -> String {
+    fn get_file_name(dir_name: &String, idx: u32) -> String {
         //println!("path: {:?}, file: {:?}, process_id: {}", p.parent(), p.file_name(), std::process::id());
-        let log_file_name = format!("{}-{}-{}.log", file_name, std::process::id(), idx);
-        println!("roll files new file name is: {}", log_file_name);
+        let exec_path = std::env::current_exe().unwrap_or(PathBuf::from("/tmp/unknow_file"));
+        let exe = exec_path.as_path().file_name().unwrap();
+        let log_file_name = format!("{}/{}-{}-{}.log", dir_name, exe.to_str().unwrap(), std::process::id(), idx);
+        println!("new file name is: {}", log_file_name);
 
         log_file_name
     }
@@ -50,7 +54,7 @@ impl FileAppender {
                 //println!("file modify time : {:?}", metadata.modified());
                 if metadata.len() > self.log_max_size {
                     self.roll_index += 1;
-                    self.file_name = FileAppender::get_file_name(&self.base_name, self.roll_index);
+                    self.file_name = FileAppender::get_file_name(&self.base_dir, self.roll_index);
                     let file = OpenOptions::new().create(true)
                                                 .write(true)
                                                 .append(true)
@@ -60,7 +64,7 @@ impl FileAppender {
                 }
             },
             Err(e) => {
-                println!("error occur in get metadata of {}, e: {:?}", self.file_name, e);
+                panic!("error occur in get metadata of {}, e: {:?}", self.file_name, e);
             }
         }
     }
@@ -81,7 +85,6 @@ impl FileAppender {
                 return r.map_err(|e| e.to_string());
             },
             None => {
-                println!("file is not exist!");
                 panic!("file is not exist!");
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,10 @@ extern crate toml;
 #[macro_use]
 extern crate lazy_static;
 
-//use strfmt::strfmt;
-
-//use std::collections::HashMap;
 use std::time::{Duration, Instant};
 use std::thread;
 use std::path::PathBuf;
 use std::io;
-
-//use std::sync::Arc;
 
 mod event;
 mod config;
@@ -25,14 +20,10 @@ mod writer;
 mod logger;
 mod appender;
 
-//use self::event::Event;
-//use self::writer::Writer;
-//use self::filter::Filters;
 use self::logger::{send_event};
 
 fn main() {
     config::Config::create_instance(Some(PathBuf::from("./")));
-    //let filters = Filters::generate_by_config().get_filter();
 
     let mut ths = Vec::new();
 
@@ -46,9 +37,9 @@ fn main() {
                 move || {
 
                     let d_now = Instant::now();
-                    for _ in 0..t1 {
-                        log_debug!("1234567890-=dfghjkl;'kald;ngtohbjgbtesting {}...99=====.{}....mmmmmmm{}m", 1,2,3);
-                        log_error!("1234567890-=dfghjkl;'kald;ngtohbjgbtesting {}...99=====.{}....mmmmmmm{}m", 1,2,3);
+                    for idx in 0..t1 {
+                        log_debug!("1234567890-=dfghjkl;'kald;ngtohbjgbtesting {}...99=====.{}....mmmmmmm{}m .... {}", 1,2,3, idx);
+                        log_error!("1234567890-=dfghjkl;'kald;ngtohbjgbtesting {}...99=====.{}....mmmmmmm{}m ********* {}", 1,2,3, idx);
                     }
                     println!("consume time is : {}", d_now.elapsed().as_millis());
 
@@ -59,10 +50,9 @@ fn main() {
         let input = io::stdin().read_line(&mut buf);
         if input.is_ok() {
             println!("consume time is : {} msec", m_now.elapsed().as_millis());
+            std::process::exit(0);
         }
         thread::sleep(Duration::from_micros(1u64));
     };
-
-    //th2.join();
 }
 
